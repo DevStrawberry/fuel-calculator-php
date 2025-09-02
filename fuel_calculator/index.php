525ffdd4d3404e1d09b966b7e23f6a5b03f41fc4
Dr. Raposa <matheustrindade05@outlook.com>
2025-09-02 08:45:31 -0300

    Update index.php

--- fuel_calculator/index.php
+++ fuel_calculator/index.php
@@ -5,31 +5,47 @@
     <title>Fuel Calculator</title>
 </head>
 <body>
-<form action="index.php" method="post">
-    <p>
-        Alcool value: <input type="number" name="alcool" id="alcool">
-    </p>
-    <p>
-        Gasoline value: <input type="number" name="gasoline" id="gasoline">
-    </p>
-    <p>
-        <input type="submit" name="calcular" placeholder="Calcular">
-    </p>
-    <script>
-        function validarValor() {
-            const inputAlcool = document.getElementById('alcool');
-            const alcoolValue = inputAlcool.value;
-            const inputGasoline = document.getElementById('gasoline');
-            const gasolineValue = inputGasoline.value;
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $alcool = $_POST['alcool'];
+        $gasoline = $_POST['gasoline'];
 
-            if (isNaN(alcoolValue && gasolineValue)) {
-                console.log("Erro: valores inseridos não são números;");
+        if (is_numeric($alcool) && is_numeric($gasoline) && $gasoline > 0) {
+            $resultado = $alcool / $gasoline;
+            if ($resultado < 0.7) {
+                echo "Abasteça com alcool. <br><br>";
+            } else {
+                echo "Abasteça com gasolina. <br><br>";
             }
+        } else {
+            echo "Erro: Por favor insira valores númericos válidos. <br><br>";
         }
-    </script>
-    <?php
+    }
+?>
+<script>
+    function validarValor() {
+        const inputAlcool = document.getElementById('alcool');
+        const alcoolValue = parseFloat(inputAlcool.value);
+        const inputGasoline = document.getElementById('gasoline');
+        const gasolineValue = parseFloat(inputGasoline.value);
 
-    ?>
+        if (isNaN(alcoolValue)  || isNaN(gasolineValue)) {
+            alert("Erro: valores inseridos não são números;");
+            return false;
+        }
+        return true;
+    }
+</script>
+<form action="index.php" method="post" onsubmit="return validarValor()">
+    <p>
+        Alcool value: <input type="number" name="alcool" id="alcool" step="0.01" required>
+    </p>
+    <p>
+        Gasoline value: <input type="number" name="gasoline" id="gasoline" step="0.01" required>
+    </p>
+    <p>
+        <input type="submit" name="calcular" value="Calcular">
+    </p>
 </form>
 </body>
 </html>
